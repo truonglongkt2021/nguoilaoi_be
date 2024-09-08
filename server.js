dd5713d6efaa5274c941143de596c379bc2e0687
truong long <truonglongkt2021@gmail.com>
2024-09-08 23:35:06 +0700

    a

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const http = require('http');
 const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+
+// Thiết lập CORS cho Socket.IO
+const io = socketIo(server, {
+  cors: {
+    origin: "https://nguoilaoi.amazingtech.cc",  // Chỉ định domain bạn muốn cho phép
+    methods: ["GET", "POST"],  // Các phương thức được phép
+    credentials: true,  // Cho phép gửi thông tin xác thực (cookies, HTTP headers...)
+  }
+});
 
 io.on('connection', (socket) => {
   console.log('New client connected', socket.id);
@@ -21,4 +29,3 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3333, () => console.log('Signaling server running on port 3333'));
- //dc
