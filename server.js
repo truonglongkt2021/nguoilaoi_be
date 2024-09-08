19964a8e1390c52e32d8ee0fab8b4b4450fc4ca3
truong long <truonglongkt2021@gmail.com>
2024-09-09 02:01:28 +0700

    a

--- server.js
+++ server.js
@@ -4,32 +4,17 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
-// Thiết lập CORS và cấu hình Socket.IO để tránh timeout
+// Thiết lập CORS cho Socket.IO
 const io = socketIo(server, {
   cors: {
-    origin: "*",  // Thay thế bằng domain của bạn
-    methods: ["GET", "POST"],
-    credentials: true
-  },
-  pingInterval: 25000,  // Thời gian giữa mỗi lần ping (25 giây)
-  pingTimeout: 60000    // Thời gian chờ phản hồi trước khi timeout (60 giây)
+    origin: "*",  // Chỉ định domain bạn muốn cho phép
+    methods: ["GET", "POST"],  // Các phương thức được phép
+    credentials: true,  // Cho phép gửi thông tin xác thực (cookies, HTTP headers...)
+  }
 });
 
 io.on('connection', (socket) => {
-  console.log('New client connectedsssssssssssssssss', socket.id);
-
-  socket.on('join-room', (roomId) => {
-    const room = io.sockets.adapter.rooms.get(roomId);
-    const numClients = room ? room.size : 0;
-
-    if (numClients < 3) {
-      socket.join(roomId);
-      console.log(`User ${socket.id} joined room ${roomId}`);
-      socket.to(roomId).emit('new-user', socket.id);
-    } else {
-      socket.emit('room-full', 'Room is full, max 3 users allowed');
-    }
-  });
+  console.log('New client connected', socket.id);
 
   socket.on('signal', (data) => {
     io.to(data.to).emit('signal', {
